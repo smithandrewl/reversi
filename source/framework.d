96f32e32c49c5ba053d9059885ac753a1855d052
Andrew Smith <smith.andrew.l@gmail.com>
2019-12-14 16:35:37 -0600

    Game board now contains an 8x8 board of cells.

--- source/framework.d
+++ source/framework.d
@@ -9,10 +9,12 @@ class Game {
 class GameData {
     private int m_width;
     private int m_height;
+    private bool m_game_over;
 
     this(int width, int height) {
         m_width = width;
         m_height = height;
+        m_game_over = false;
     }
 
     int width() {
@@ -21,6 +23,14 @@ class GameData {
 
     int height() {
         return m_height;
+    }
+
+    bool isGameOver() {
+        return m_game_over;
+    }
+
+    void isGameOver(bool status) {
+        m_game_over = status;
     }
 }
 
@@ -31,10 +41,19 @@ enum CellState {
 }
 
 class GameBoard {
+    private CellState[][] m_cells;
 
+    CellState[][] cells() {
+        return m_cells;
+    }
+
+    this() {
+        m_cells = new CellState[][](8,8);
+
+        for (int i=0; i < 8; i++){
+            for (int j = 0; j < 8; j++) {
+                m_cells[i][j] = CellState.FREE;
+            }
+        }
+    }
 }
-
-class GameRenderer {
-
-}
-
--- source/tests/game_board_tests.d
+++ source/tests/game_board_tests.d
@@ -10,3 +10,40 @@ unittest {
     800.assertEqual(gameData.width, "Width accessor returned " ~ to!string(gameData.width) ~ " instead of 800");
     600.assertEqual(gameData.height, "Height accessor returned " ~ to!string(gameData.height) ~ " instead of 600");
 }
+
+unittest {
+    GameData gameData = new GameData(800, 600);
+
+    gameData.isGameOver.assertEqual(false, "The game was marked as being over just after construction");
+}
+
+unittest {
+    GameData gameData = new GameData(800, 600);
+
+    gameData.isGameOver = true;
+
+    gameData.isGameOver.assertEqual(true, "The game was marked as not being over after being set to over");
+}
+
+unittest {
+    GameBoard gameBoard = new GameBoard();
+
+    CellState[][] cells = gameBoard.cells;
+
+    bool correct = true;
+
+    for(int i = 0; i < 8; i++) {
+        if(!correct) {
+            break;
+        }
+
+        for(int j = 0; j < 8; j++) {
+            if(cells[i][j] != CellState.FREE) {
+                correct = false;
+                break;
+            }
+        }
+    }
+
+    correct.assertEqual(true, "After initialization, the game board had non-free cells");
+}
