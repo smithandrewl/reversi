b6ae86986918d809f7493232dcf43ba529dd2e8d
Andrew Smith <smith.andrew.l@gmail.com>
2019-12-14 14:45:14 -0600

    Added a real test of the GameData accessors to the game_board_tests class.

--- source/tests/game_board_tests.d
+++ source/tests/game_board_tests.d
@@ -2,11 +2,11 @@ module game_board_tests;
 
 import std.stdio;
 import dunit.toolkit;
+import std.conv;
 
+import framework;
 unittest {
-    "d".assertEndsWith("d", "d should end with d");
-}
-
-unittest {
-    "e".assertEndsWith("d","e should end with d");
+    GameData gameData = new GameData(800, 600);
+    800.assertEqual(gameData.width, "Width accessor returned " ~ to!string(gameData.width) ~ " instead of 800");
+    600.assertEqual(gameData.height, "Height accessor returned " ~ to!string(gameData.height) ~ " instead of 600");
 }
