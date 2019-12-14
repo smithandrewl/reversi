d9e191d715e5bb34f6879e0ca516451be5631aff
Andrew Smith <smith.andrew.l@gmail.com>
2019-12-14 13:28:08 -0600

    Added a constructor and two readonly properties (width and height) to the GameData class.

--- source/framework.d
+++ source/framework.d
@@ -7,7 +7,21 @@ class Game {
 }
 
 class GameData {
+    private int m_width;
+    private int m_height;
 
+    this(int width, int height) {
+        m_width = width;
+        m_height = height;
+    }
+
+    int width() {
+        return m_width;
+    }
+
+    int height() {
+        return m_height;
+    }
 }
 
 enum CellState {
