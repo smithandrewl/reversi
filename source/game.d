2ad3221dc08774367a9101ec30085546e1582b36
Andrew Smith <smith.andrew.l@gmail.com>
2019-12-24 12:27:21 -0600

    White board pieces are placed on left mouse click.

--- source/game.d
+++ source/game.d
@@ -3,6 +3,7 @@ module game;
 import game_renderer;
 import game_data;
 import std.stdio: stdout;
+import game_board: CellState;
 
 class Game {
     private GameData     m_gameData;
@@ -20,7 +21,16 @@ class Game {
     }
 
     void leftClick(int x, int y) {
-        stdout.writefln("Left click at screen pos (%d, %d)", x, y);
+
+        int padding = 25;
+
+        int m_cell_width  = (m_gameData.width  - (2 * padding)) / 8;
+        int m_cell_height = (m_gameData.height - (2 * padding)) / 8;
+
+        int cellX = (x - padding) / m_cell_width;
+        int cellY = (y - padding) / m_cell_height;
+
+        m_gameData.gameBoard.set(cellY, cellX, CellState.WHITE);
 
     }
 }
